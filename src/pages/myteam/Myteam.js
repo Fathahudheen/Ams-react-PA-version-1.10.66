76e367b5bf6e97ec9744edf6408ee7396a5328fe
rithu10 <rithu7025@gmail.com>
2023-04-26 10:40:10 +0530

    myteam added

--- src/pages/myteam/Myteam.js
+++ src/pages/myteam/Myteam.js
@@ -1,8 +1,13 @@
 import React from 'react'
+import ViewTable from './ViewTable/ViewTable'
+// import 'Index.css'
+
 
 const Myteam = () => {
   return (
-    <div><h1>My Team</h1></div>
+    <>
+    <ViewTable/>
+    </>
   )
 }
 
