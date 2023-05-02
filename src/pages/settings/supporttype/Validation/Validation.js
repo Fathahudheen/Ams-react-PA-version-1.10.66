a2d53dcfa598889f753a822f65a0eca8b3b7601e
SANJAI BABU S <bsanjay235@gmail.com>
2023-05-02 22:45:35 +0530

    again updated support type

--- src/pages/settings/supporttype/Validation/Validation.js
+++ src/pages/settings/supporttype/Validation/Validation.js
@@ -2,5 +2,4 @@ import * as Yup from "yup";
 
 export const ValidSchema = Yup.object({
   name: Yup.string().min(1).max(30).required("Please enter the name"),
-  password: Yup.string().min(8).required("Please enter password*"),
 });
